b6fa62feb9bb9c8b432d611054b05a3d90af8f0b
leiyu-97 <ly18824110284@gmail.com>
2025-01-03 11:00:22 +0800

    feat: 创建 enshrouded server

---
+++ .env.tpl
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+export SERVER_PASSWORD=""
+export ADMIN_PASSWORD=""
--- game-server.sh
+++ game-server.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
-./password.sh || exit
-export DOCKER_BUILDKIT=1 
+export DOCKER_BUILDKIT=1
 
-usage(){
-cat <<'EOF'
+usage() {
+  cat <<'EOF'
 Usage: ./game-server.sh [option] <command>
   -n, --name                  服务名（palworld, teamspeak）
   -h, --help                  帮助
@@ -18,61 +17,71 @@ Usage: ./game-server.sh [option] <command>
 EOF
 }
 
-DIR="$(dirname "$0")"
+ROOT_DIR=$(readlink -f "$(dirname "$0")")
 # 默认值
 NAME=docker-example
 
 while [ $1 ]; do
-    case "$1" in
-        -n|--name)
-          NAME=$2
-          shift 2
-          ;;
-        -h|--help)
-          usage
-          exit
-          ;;
-        *)
-          break
-          ;;
-    esac
+  case "$1" in
+  -n | --name)
+    NAME=$2
+    shift 2
+    ;;
+  -h | --help)
+    usage
+    exit
+    ;;
+  *)
+    break
+    ;;
+  esac
 done
 
-if [ ! $1 ];
-then
+if [ ! $1 ]; then
   usage
   exit 1
 fi
 CMD=$1
 shift
 
-export NAME=$NAME
 echo NAME: $NAME
+if [ "$CMD" = "status" ]; then
+  CMD="logs"
+fi
+
+DIR="${ROOT_DIR}/servers/${NAME}/"
+
+cat <<EOF >.temp_env
+DIR=${DIR}
+ROOT_DIR=${ROOT_DIR}
+EOF
+
+COMMON_PARAMS="--env-file ./.env --env-file ./.temp_env -p $NAME -f $DIR/docker-compose.yml"
 
 case "$CMD" in
-    start)
-        docker compose -p $NAME -f "${DIR}/servers/${NAME}/docker-compose.yml" up -d --build
-        docker compose -f "${DIR}/servers/${NAME}/docker-compose.yml" logs -t -f
-        ;;
-    stop)
-        docker compose  -p $NAME -f "${DIR}/servers/${NAME}/docker-compose.yml" down
-        ;;
-    restart)
-        docker compose  -p $NAME -f "${DIR}/servers/${NAME}/docker-compose.yml" down
-        docker compose  -p $NAME -f "${DIR}/servers/${NAME}/docker-compose.yml" up -d --build
-        docker compose -f "${DIR}/servers/${NAME}/docker-compose.yml" logs -t -f
-        ;;
-    remove)
-        docker compose  -p $NAME -f "${DIR}/servers/${NAME}/docker-compose.yml" down
-        docker image rm $(docker images --format "{{.Repository}}" | grep $NAME)
-        ;;
-    shell)
-        docker exec -ti $NAME /bin/bash
-        ;;
-    logs)
-        docker compose -f "${DIR}/servers/${NAME}/docker-compose.yml" logs -t -f
-        ;;
-    *)
-      usage
-      ;;
+start)
+  docker compose $COMMON_PARAMS up -d --build
+  docker compose $COMMON_PARAMS logs -t -f
+  ;;
+stop)
+  docker compose $COMMON_PARAMS down
+  ;;
+restart)
+  docker compose $COMMON_PARAMS down
+  docker compose $COMMON_PARAMS up -d --build
+  docker compose $COMMON_PARAMS logs -t -f
+  ;;
+remove)
+  docker compose $COMMON_PARAMS down
+  docker image rm $(docker compose $COMMON_PARAMS images --quiet)
+  ;;
+shell)
+  docker exec -ti $NAME /bin/bash
+  ;;
+logs)
+  docker compose $COMMON_PARAMS logs -t -f
+  ;;
+*)
+  usage
+  ;;
 esac
